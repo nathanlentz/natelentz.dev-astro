dcc8d75282f4fe727510e03c29a40cfd794e245c
Nate Lentz <nlentz@ostusa.com>
2023-08-27 19:46:38 -0400

    add favicon

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,9 +4,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Nate Lentz",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Developer, Father, Husband | Nate Lentz",
+	title: "Nate Lentz -- Dev, Father, Husband",
 	// Meta property used as the default description meta property
-	description: "Personal blog of Nate Lentz",
+	description: "A blog by Nate Lentz",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
